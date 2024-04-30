9392dc2b52185ebec040c98108bae881fb4bbd68
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-04-30 16:48:45 +0530

    Program for learning different datatypes in java

---
+++ Basics/variable1.java
@@ -0,0 +1,31 @@
+
+//Program for learning different datatypes in java
+
+public class variable1 {
+    
+    public static void main(String[] args) {
+        
+        int num1 = 9;
+        byte by = 127;
+        short sh = 558;
+        long l = 5854l;
+
+        float f = 5.8f;
+        double d = 5.8;
+
+        char c= 'k';
+
+        boolean b= true;
+
+        System.out.println(num1);
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+
+
+    }
+}
