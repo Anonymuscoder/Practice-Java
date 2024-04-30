1f3e3f1eed9a010262513215e642fc67ec3745ec
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-04-30 15:56:14 +0530

    Program for variable

---
+++ Basics/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    
+    public static void main(String[] args) {
+        int num1 =3;
+        int num2 = 5;
+        int result= num1+num2;
+        System.out.println(result);
+
+    }
+}
