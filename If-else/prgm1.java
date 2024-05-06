cf77d55478b8ae48a1db780e87d71c105d46e605
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-05-06 23:03:06 +0530

    Program using If-else statement

---
+++ If-else/prgm1.java
@@ -0,0 +1,16 @@
+
+//Program for finding greater value among two values 
+
+public class prgm1
+{
+    public static void main(String[] args) {
+        
+        int x =5;
+        int y=7;
+
+        if(x>y)
+            System.out.println(x+" is greater than "+y);
+        else
+            System.out.println(y+" is greater than "+x);
+    }
+}
