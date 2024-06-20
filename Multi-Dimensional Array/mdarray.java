b83fa8d3a57b4029d5cda7201da292ecb8c1406d
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-20 21:10:57 +0530

    code for implementing MultiDimensional Array in java and performing operations on it

---
+++ Multi-Dimensional Array/mdarray.java
@@ -0,0 +1,33 @@
+class mdarray{
+    public static void main(String[] args) {
+        
+        int nums [] [] = new int [3] [4];
+
+        System.out.println("Printing created empty array...");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                System.out.print(nums[i][j]+ " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+         
+        System.out.println("fetching values using Math.random() by performing type casting....");
+        for(int i=0; i<3; i++){ //fetching elements using Math library in java
+            for(int j=0; j<4; j++){
+                nums [i] [j]=(int)(Math.random() * 10);// type casting of double into integer
+            }
+        }
+        System.out.println();
+
+        System.out.println("Printing array with the values");
+        for(int i=0; i<3; i++){
+            for(int j=0; j<4; j++){
+                System.out.print(nums[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
