6e90e881f0c6cb3564ee3227b04a9ea7d5cd7a2d
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-06-24 22:32:26 +0530

    code for demonstrating string in java

---
+++ String/strng.java
@@ -0,0 +1,16 @@
+
+//code for demonstrating String in java
+
+class strng{
+    public static void main(String[] args) {
+        
+        String name = new String("Rajesh");
+
+        System.out.println(name);
+        System.out.println(name.hashCode());
+        System.out.println(name.concat("kumar"));
+        System.out.println(name.indexOf('e'));
+        System.out.println(name.length());
+
+    }
+}
