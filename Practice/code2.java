5be74fb924de2e62882f734420115f568d705358
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-11-11 21:36:47 +0530

    program for declaring variables and printing their values

---
+++ Practice/code2.java
@@ -0,0 +1,18 @@
+//Q.Declare and initialize variables of type int, float, char, and boolean, then print their values.
+
+class code2 {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 17;
+        String str = "Harry";
+        //char c = 'Harry'  ..gives error
+        char c = 'H';
+        boolean d = true;
+        System.out.println( a+" "+b+" "+c+" "+d+" "+str);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(str);
+    }
+}
