51a45a1c788c1b43405e4bf017014d457a664939
Anonymuscoder <ashutoshpathak.rp1710@gmail.com>
2024-12-27 17:23:33 +0530

    code for printing a number pyramid

---
+++ Practice/code26.java
@@ -0,0 +1,18 @@
+//code for printing a number pyramid
+
+import java.util.Scanner;
+
+public class code26 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows and columns");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
